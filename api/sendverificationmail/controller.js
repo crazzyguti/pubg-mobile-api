daed7f54f31e6ef4543b89b38247d520a2290f08
Jaydev Thomke <jaydevthomke.in@gmail.com>
2018-09-13 22:14:31 -0700

    Prod

--- api/sendverificationmail/controller.js
+++ api/sendverificationmail/controller.js
@@ -1,36 +1,36 @@
 const sendGrid = require('sendgrid').mail;
-const sg = require('sendgrid')(process.env.SEND_GRID_API_KEY);
+// const sg = require('sendgrid')(process.env.SEND_GRID_API_KEY);
 var verifier = require('email-verify');
       var infoCodes = verifier.infoCodes;
       
 const sendVerificationEmail = (to, token) => {
     const hostUrl = process.env.HOST_URL;
-    const request = sg.emptyRequest({
-      method: "POST",
-      path: "/v3/mail/send",
-      body: {
-        personalizations: [
-          {
-            to: [
-              {
-                email: to
-              }
-            ],
-            subject:"Verify Your Email"
-          }
-        ],
-        from: {
-          email: "no-reply@example.com"
-        },
-        content: [
-            {
-                type: 'text/html',
-                value: `Click <a href="${hostUrl}/verification?token=${token}&email=${to}">here</a> to verify your email.
-                <p>Please ignore this email if you have not registered to Ads App</p>`
-            }
-        ]
-      }
-    });
+    // const request = sg.emptyRequest({
+    //   method: "POST",
+    //   path: "/v3/mail/send",
+    //   body: {
+    //     personalizations: [
+    //       {
+    //         to: [
+    //           {
+    //             email: to
+    //           }
+    //         ],
+    //         subject:"Verify Your Email"
+    //       }
+    //     ],
+    //     from: {
+    //       email: "no-reply@example.com"
+    //     },
+    //     content: [
+    //         {
+    //             type: 'text/html',
+    //             value: `Click <a href="${hostUrl}/verification?token=${token}&email=${to}">here</a> to verify your email.
+    //             <p>Please ignore this email if you have not registered to Ads App</p>`
+    //         }
+    //     ]
+    //   }
+    // });
     var send = require('gmail-send')({
       //var send = require('../index.js')({
       user: process.env.ADMIN_EMAIL,
