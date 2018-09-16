6727ce92091903e5f9c9de7439353749cdec5317
Jaydev Thomke <jaydevthomke.in@gmail.com>
2018-09-16 10:21:36 -0700

    logs

--- api/matches/index.js
+++ api/matches/index.js
@@ -5,7 +5,7 @@ const passport = require('passport');
 const MatchesController = require('./controller');
   // POST /verication?token=[string]&email=[string]
 router.get('/matches/listing/', passport.authenticate('jwt', { session: false }), MatchesController.getMatches);
-router.post('/matches/verifypayment/:userId/:mathcId', MatchesController.verifypayment);
+router.post('/matches/verifypayment/:userId/:matchId', MatchesController.verifypayment);
 router.post('/matches/deletePlayer/', passport.authenticate('jwt', { session: false }), MatchesController.deletePlayer);
 router.post('/matches/entry/', passport.authenticate('jwt', { session: false }), MatchesController.createMatchEntry);
 
