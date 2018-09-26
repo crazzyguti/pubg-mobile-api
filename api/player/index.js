fa673b074e1a12296051a24de8d3fff0e3551330
Jaydev Thomke <jaydevthomke.in@gmail.com>
2018-09-25 21:40:42 -0700

    asdasdsad

--- api/player/index.js
+++ api/player/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
   , router = express.Router();
   const playerController = require('./controller');
+  const passport = require('passport');
 
 // const passport = require('passport');
 // POST /verication?token=[string]&email=[string]
-router.get('/player/profile/', playerController.getPlayerProfile);
+router.get('/player/profile/', passport.authenticate('jwt', { session: false }), playerController.getPlayerProfile);
 
 module.exports = router;
